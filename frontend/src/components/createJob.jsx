f8dcb87eb2f74a2972c03238e46e737590ea9c88
HarishLakshmanan <your-harish.lakshmanan@outlook.com>
2025-05-18 19:12:26 +0530

    css update

--- frontend/src/components/createJob.jsx
+++ frontend/src/components/createJob.jsx
@@ -50,22 +50,22 @@ export default function CreateJobModal({ isOpen, onClose }) {
 
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 backdrop-sm">
-            <div className="bg-white rounded-xl p-6 w-300 h-160 max-w-3xl shadow-lg relative">
-                <h2 className="text-xl font-semibold mb-4 text-center">Create Job Opening</h2>
+            <div className="bg-white rounded-xl p-6 w-300 h-160 max-w-3xl shadow-lg relative ">
+                <h2 className="text-[24px] font-semibold mb-7 text-center">Create Job Opening</h2>
 
                 <form className="grid grid-cols-2 gap-4" onSubmit={handleSubmit}>
-                    <div className="flex flex-col gap-1">
+                    <div className="flex flex-col gap-1 text-[20px]">
                         <p>Job Title</p>
-                        <input name="title" value={formData.title} onChange={handleChange} type="text" placeholder="Job Title" className={`border-2 rounded px-4 py-2 transition-colors duration-200 ${formData.title ? 'border-[#222222]' : 'border-[#BCBCBC]'
+                        <input name="title" value={formData.title} onChange={handleChange} type="text" placeholder="Job Title" className={`border-2 text-[18px] rounded px-4 py-2 transition-colors duration-200 ${formData.title ? 'border-[#222222]' : 'border-[#BCBCBC]'
                             }`} />
                     </div>
-                    <div className="flex flex-col gap-1">
+                    <div className="flex flex-col gap-1 text-[20px]">
                         <p>Company Name</p>
-                        <input name="company" value={formData.company} onChange={handleChange} type="text" placeholder="Amazon, Microsoft, Swiggy" className={`border-2 rounded px-4 py-2 transition-colors duration-200 ${formData.company ? 'border-[#222222]' : 'border-[#BCBCBC]'
+                        <input name="company" value={formData.company} onChange={handleChange} type="text" placeholder="Amazon, Microsoft, Swiggy" className={`border-2 text-[18px] rounded px-4 py-2 transition-colors duration-200 ${formData.company ? 'border-[#222222]' : 'border-[#BCBCBC]'
                             }`} />
                     </div>
 
-                    <div className="flex flex-col gap-1">
+                    <div className="flex flex-col gap-1 text-[20px]">
                         <p>Location</p>
                         <input
                             type="text"
@@ -73,15 +73,15 @@ export default function CreateJobModal({ isOpen, onClose }) {
                             value={formData.location}
                             onChange={handleChange}
                             placeholder="Enter preferred location"
-                            className={`border-2 rounded px-4 py-2 transition-colors duration-200 ${formData.location ? 'border-[#222222]' : 'border-[#BCBCBC]'
+                            className={`border-2 text-[18px] rounded px-4 py-2 transition-colors duration-200 ${formData.location ? 'border-[#222222]' : 'border-[#BCBCBC]'
                                 }`}
                         />
                     </div>
 
 
-                    <div className="flex flex-col gap-1">
+                    <div className="flex flex-col gap-1 text-[20px]">
                         <p>Job Type</p>
-                        <select name="jobType" value={formData.jobType} onChange={handleChange} className={`border-2  rounded px-4 py-2 transition-colors duration-200 ${formData.jobType ? 'border-[#222222]' : 'border-[#BCBCBC]'
+                        <select name="jobType" value={formData.jobType} onChange={handleChange} className={`border-2 text-[18px] rounded px-4 py-2 transition-colors duration-200 ${formData.jobType ? 'border-[#222222]' : 'border-[#BCBCBC]'
                             }`}>
                             <option value="">Select Type</option>
                             <option value="Internship">Internship</option>
@@ -91,7 +91,7 @@ export default function CreateJobModal({ isOpen, onClose }) {
                         </select>
                     </div>
 
-                    <div className="flex flex-col gap-1">
+                    <div className="flex flex-col gap-1 text-[20px]">
                         <p>Salary Range</p>
                         <div className="flex gap-2">
                             <div className="relative w-full">
@@ -102,7 +102,7 @@ export default function CreateJobModal({ isOpen, onClose }) {
                                     onChange={handleChange}
                                     type="number"
                                     placeholder="₹0"
-                                    className={`pl-10 pr-3 py-2 w-full border-2 rounded transition-colors duration-200 ${formData.salaryMin ? 'border-[#222222]' : 'border-[#BCBCBC]'
+                                    className={`pl-10 pr-3 py-2 w-full border-2 text-[18px] rounded transition-colors duration-200 ${formData.salaryMin ? 'border-[#222222]' : 'border-[#BCBCBC]'
                                         }`}
                                 />
                             </div>
@@ -119,7 +119,7 @@ export default function CreateJobModal({ isOpen, onClose }) {
                                     onChange={handleChange}
                                     type="number"
                                     placeholder="₹12,00,000"
-                                    className={`pl-10 pr-3 py-2 w-full border-2 rounded transition-colors duration-200 ${formData.salaryMax ? 'border-[#222222]' : 'border-[#BCBCBC]'
+                                    className={`pl-10 pr-3 py-2 w-full border-2 text-[18px] rounded transition-colors duration-200 ${formData.salaryMax ? 'border-[#222222]' : 'border-[#BCBCBC]'
                                         }`}
                                 />
                             </div>
@@ -127,15 +127,15 @@ export default function CreateJobModal({ isOpen, onClose }) {
                         </div>
                     </div>
 
-                    <div className="flex flex-col gap-1">
+                    <div className="flex flex-col gap-1 text-[20px]">
                         <p>Application Deadline</p>
-                        <input name="deadline" value={formData.deadline} onChange={handleChange} type="date" className={`border-2  rounded px-4 py-2 transition-colors duration-200 ${formData.deadline ? 'border-[#222222]' : 'border-[#BCBCBC]'
+                        <input name="deadline" value={formData.deadline} onChange={handleChange} type="date" className={`border-2 text-[18px] rounded px-4 py-2 transition-colors duration-200 ${formData.deadline ? 'border-[#222222]' : 'border-[#BCBCBC]'
                             }`} />
                     </div>
 
-                    <div className="col-span-2 flex flex-col gap-1">
+                    <div className="col-span-2 flex flex-col gap-1 text-[20px]">
                         <p>Job Description</p>
-                        <textarea name="description" value={formData.description} onChange={handleChange} className={`w-full h-30 px-4 pt-2 border-2 rounded transition-colors duration-200 ${formData.description ? 'border-[#222222]' : 'border-[#BCBCBC]'
+                        <textarea name="description" value={formData.description} onChange={handleChange} className={`w-full h-30 px-4 pt-2 border-2 text-[18px] rounded transition-colors duration-200 ${formData.description ? 'border-[#222222]' : 'border-[#BCBCBC]'
                             }`} placeholder="Please share a description to let the candidate know more about the job role" />
                     </div>
 
